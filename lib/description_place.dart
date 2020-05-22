f64d49e6e617adb5179566317b233d3c782cb782
Héctor Tello <hectortllo@gmail.com>
2020-05-22 17:25:31 -0600

    Se utilizaron variables en lugar de andar hard codeando código

--- lib/description_place.dart
+++ lib/description_place.dart
@@ -1,8 +1,37 @@
 import 'package:flutter/material.dart';
 
 class DescriptionPlace extends StatelessWidget{
+  String namePlace;
+  int stars;
+  String descriptionPlace;
+
+  DescriptionPlace(this.namePlace, this.stars, this.descriptionPlace);
+
   @override
   Widget build(BuildContext context) {
+
+    final star_half = Container(
+      margin: EdgeInsets.only(
+        top: 323.0,
+        right: 3.0
+      ),
+      child: Icon(
+        Icons.star_half,
+        color: Color(0xFFf2C611),
+      )
+    );
+
+    final star_border = Container(
+      margin: EdgeInsets.only(
+        top: 323.0,
+        right: 3.0
+      ),
+      child: Icon(
+        Icons.star_border,
+        color: Color(0xFFf2C611),
+      )
+    );
+
     final star = Container(
       margin: EdgeInsets.only(
         top: 323.0,
@@ -14,16 +43,18 @@ class DescriptionPlace extends StatelessWidget{
       ),
     );
 
-    final desription = Container(
+    final description = Container(
       margin: EdgeInsets.only(
         top: 20.0,
         right: 20.0,
         left: 20.0
       ),
-      child: Text(
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum",
+      child: new Text(
+        descriptionPlace,
         style: TextStyle(
-          fontSize: 11.0,
+          fontSize: 16.0,
+          fontWeight: FontWeight.bold,
+          color: Color(0xFF56575a)
         ),
         textAlign: TextAlign.justify,
       ),
@@ -38,7 +69,7 @@ class DescriptionPlace extends StatelessWidget{
             right: 20.0
           ),
           child: Text(
-            "Diwili Ella",
+            namePlace,
             style: TextStyle(
               fontSize: 30.0,
               fontWeight: FontWeight.w900
@@ -52,20 +83,17 @@ class DescriptionPlace extends StatelessWidget{
             star,
             star,
             star,
-            star
+            star_border
           ],
         ),
       ],
     );
     
-    final listView = Container(
-      child: Column(
-        children: <Widget>[
-          title_stars,
-          desription
-        ],
-      )
+    return Column(
+      children: <Widget>[
+        title_stars,
+        description
+      ],
     );
-    return listView;
   } 
 }
--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,7 @@ void main() {
 
 class MyApp extends StatelessWidget { 
   // This widget is the root of your application.
+  String description = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.";
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -31,7 +32,7 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: Text("Hola mundo Feliz"),
         ),
-        body: new DescriptionPlace(),
+        body: new DescriptionPlace("Bahamas", 4, description),
       )
       //MyHomePage(title: 'Flutter Demo Home Page'),
     );
