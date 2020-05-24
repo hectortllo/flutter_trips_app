5f5fe421c5ae7664cb46e07ec819a5f7e948b1a9
Héctor Tello <hectortllo@gmail.com>
2020-05-24 17:43:27 -0600

    Se agregó el bottom navigation bar

---
+++ lib/home_trips.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'description_place.dart';
+import 'review_list.dart';
+import 'header_appbar.dart';
+
+class HomeTrips extends StatelessWidget {
+  final String description = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.";
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: <Widget>[
+        ListView(
+          children: <Widget>[
+            DescriptionPlace("Bahamas", 4, description),
+            ReviewList()
+          ],
+        ),
+        HeaderAppBar()
+      ],
+    );
+  }
+  
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'description_place.dart';
-import 'review_list.dart';
-import 'header_appbar.dart';
+import 'platzi_trips.dart';
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget { 
   // This widget is the root of your application.
-  final String description = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.";
+  
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -30,20 +28,7 @@ class MyApp extends StatelessWidget {
         // closer together (more dense) than on mobile platforms.
         //visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: Scaffold(
-        //body: new DescriptionPlace("Bahamas", 4, description),
-        body: Stack(
-          children: <Widget>[
-            ListView(
-              children: <Widget>[
-                DescriptionPlace("Bahamas", 4, description),
-                ReviewList()
-              ],
-            ),
-            HeaderAppBar()
-          ],
-        ),
-      )
+      home: PlatziTrips()
       //MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
