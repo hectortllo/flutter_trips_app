b3e5002da7959bd1bee48e21fca6080eae5484e6
Héctor Tello <hectortllo@gmail.com>
2020-05-26 08:32:41 -0600

    Se actualizó el proyecto

--- lib/profile_trips.dart
+++ lib/profile_trips.dart
@@ -1,23 +1,25 @@
 import 'package:flutter/material.dart';
-import 'package:platzi_trips_app/gradient_back.dart';
-import 'user_details.dart';
-import 'card_profile_image_list.dart';
+import 'package:platzi_trips_app/profile_header.dart';
+import 'profile_places_list.dart';
+import 'profile_background.dart';
 
 class ProfileTrips extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final appbar = Stack(
-      children: <Widget>[
-        GradientBack("Profile", 380.0),
-        UserDetails()
-      ],
-    );
-
+    /*return Container(
+      color: Colors.indigo,
+    );*/
     return Stack(
       children: <Widget>[
-        appbar,
-        CardProfileImageList()
+        ProfileBackground(),
+        ListView(
+          children: <Widget>[
+            ProfileHeader(),
+            ProfilePlacesList()
+          ],
+        ),
       ],
     );
   }
+
 }
