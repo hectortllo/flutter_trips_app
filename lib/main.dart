42671a14ce086b5e8c13033de1a2f094e134ec1c
Héctor Tello <hectortllo@gmail.com>
2020-05-29 13:28:39 -0600

    1. Se crearon los archivos bloc para los dos modelos. 2. Se implementó el generic_bloc_provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:generic_bloc_provider/generic_bloc_provider.dart';
+import 'package:platzi_trips_app/User/bloc/bloc_user.dart';
 import 'platzi_trips.dart';
-import 'platzi_trips_cupertino.dart';
 
 void main() {
   runApp(MyApp());
@@ -12,26 +13,12 @@ class MyApp extends StatelessWidget {
   
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
-        //visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: PlatziTrips()
-      //MyHomePage(title: 'Flutter Demo Home Page'),
+    return BlocProvider(
+      child: MaterialApp(
+        title: "Flutter Demo",
+        home: PlatziTrips(),
+      ), 
+      bloc: UserBloc()
     );
   }
 }
