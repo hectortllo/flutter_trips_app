ef3170af97bf76b6605fdd920d94f78bdc6fd3e4
Héctor Tello <hectortllo@gmail.com>
2020-05-30 18:31:46 -0600

    Se añadió un nuevo screen para hacer el logIn con google

--- lib/main.dart
+++ lib/main.dart
@@ -3,12 +3,13 @@ import 'package:flutter/services.dart';
 import 'package:generic_bloc_provider/generic_bloc_provider.dart';
 import 'package:platzi_trips_app/User/bloc/bloc_user.dart';
 import 'platzi_trips.dart';
+import 'User/ui/screens/sign_in_screen.dart';
 
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget { 
+class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   
   @override
@@ -16,7 +17,7 @@ class MyApp extends StatelessWidget {
     return BlocProvider(
       child: MaterialApp(
         title: "Flutter Demo",
-        home: PlatziTrips(),
+        home: SignInScreen(),
       ), 
       bloc: UserBloc()
     );
