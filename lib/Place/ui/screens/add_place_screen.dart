4a6a404bc418d6bf7c6a1464311392233f59e189
Héctor Tello <hectortllo@gmail.com>
2020-06-05 22:47:14 -0600

    Avance en el screen para agregar un place

--- lib/Place/ui/screens/add_place_screen.dart
+++ lib/Place/ui/screens/add_place_screen.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:platzi_trips_app/Place/ui/widgets/title_input_location.dart';
 import 'package:platzi_trips_app/widgets/gradient_back.dart';
 import 'package:platzi_trips_app/widgets/text_input.dart';
 import 'package:platzi_trips_app/widgets/title_header.dart';
@@ -86,6 +87,15 @@ class _AddPlaceScreenState extends State<AddPlaceScreen> {
                   inputType: TextInputType.multiline, 
                   controller: _controllerDescriptionPlace,
                   maxLines: 4,
+                ),
+                Container(
+                  margin: EdgeInsets.only(
+                    top: 20.0
+                  ),
+                  child: TextInputLocation(
+                    hintText: "Add Location", 
+                    iconData: Icons.location_on, 
+                  ),
                 )
               ],
             ),
