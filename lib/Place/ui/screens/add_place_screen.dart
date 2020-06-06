c8c7cf16f5d60c077fdb7a1f8ddf3d2d1ff95e94
Héctor Tello <hectortllo@gmail.com>
2020-06-05 22:02:25 -0600

    Avance en el diseño de la screen de agregar place

--- lib/Place/ui/screens/add_place_screen.dart
+++ lib/Place/ui/screens/add_place_screen.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:platzi_trips_app/widgets/gradient_back.dart';
+import 'package:platzi_trips_app/widgets/text_input.dart';
 import 'package:platzi_trips_app/widgets/title_header.dart';
 
 class AddPlaceScreen extends StatefulWidget {
@@ -18,6 +19,9 @@ class AddPlaceScreen extends StatefulWidget {
 class _AddPlaceScreenState extends State<AddPlaceScreen> {
   @override
   Widget build(BuildContext context) {
+    final _controllerTitlePlace = TextEditingController();
+    final _controllerDescriptionPlace = TextEditingController();
+
     return Scaffold(
       body: Stack(
         children: <Widget>[
@@ -57,6 +61,34 @@ class _AddPlaceScreenState extends State<AddPlaceScreen> {
                 )
               )
             ],
+          ),
+          Container(
+            margin: EdgeInsets.only(
+              top: 120.0,
+              bottom: 20.0
+            ),
+            child: ListView(
+              children: <Widget>[
+                Container(),
+                Container(
+                  margin: EdgeInsets.only(
+                    bottom: 20.0
+                  ),
+                  child: TextInput(
+                    hintText: "Title", 
+                    inputType: null, 
+                    controller: _controllerTitlePlace,
+                    maxLines: 1,
+                  ),
+                ),
+                TextInput(
+                  hintText: "Description", 
+                  inputType: TextInputType.multiline, 
+                  controller: _controllerDescriptionPlace,
+                  maxLines: 4,
+                )
+              ],
+            ),
           )
         ],
       ),
